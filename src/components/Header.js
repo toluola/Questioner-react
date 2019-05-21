eb802ebe2f919a670cb2f7b6da86505d704b1da4
Tolulope Olaniyan <toluola7@gmail.com>
2019-05-21 16:47:47 +0100

    adjust signin form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-script-url */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React from "react";
 
 export const Header = () => {
   return (
     <div className="header">
-  <a href="/" className="logo">QUESTIONER</a>
-  <div className="header-right">
-    <a href="/signup" className='links'>CREATE ACCOUNT</a>
-    <a href="/signin" className='links'>LOGIN</a>
-  </div>
-  <a href="javascript:void(0);" className="icon" >
-    <i className="fa fa-bars"></i>
-  </a>
-</div>
-  )
-}
+      <a href="/" className="logo">
+        QUESTIONER
+      </a>
+      <a href="javascript:void(0);" className="icon">
+        <i className="fa fa-bars" />
+      </a>
+    </div>
+  );
+};
+
+// {menu.map(menus => (
+//   <div className="header-right">
+//     <a href='' className="links">
+//       {menus}
+//     </a>
+//   </div>
+// ))}
